64fb60f1133b6f1d8bb9b5d09722f57aa72441bd
James Higgins and Mohamed Warsame <james@>
2017-03-20 10:46:40 -0700

    add temperature tool

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,9 +1,58 @@
-var add = function(number1, number2) {
-	return number1 + number2;
+// var temp = function(number1) {
+// 	return number1 * 1.8 +32;
+// };
+//
+// var number1 = parseInt(prompt("Enter a temperature in celcius:"));
+//
+// alert(temp(number1));
+
+
+
+var temp = function(number1) {
+	return (number1 -32) /1.8;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a temperature in fahrenheit:"));
+
+alert(temp(number1));
 
 
-alert(add(number1,number2));
+
+
+// var add = function(number1, number2) {
+// 	return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+//
+// alert(add(number1,number2));
+
+// var subtract = function(number1, number2) {
+// 	return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(subtract(number1,number2));
+
+// var multiply = function(number1, number2) {
+// 	return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(multiply(number1,number2));
+//
+//
+// var divide = function(number1, number2) {
+// 	return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(divide(number1,number2));
